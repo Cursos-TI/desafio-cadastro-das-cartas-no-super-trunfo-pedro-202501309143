dc6148401c14702582ad8b8439f8b16876f12e7e
Pedro <202501309143@alunos.estacio.br>
2025-03-28 03:34:34 +0000

    Conclusão Desafio: nível aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    char state1;
-    char code1[4];
-    char city1[100];
-    int population1;
-    float area1;
-    float gdp1;
-    int touristSpots1;
-
-    char state2;
-    char code2[4];
-    char city2[100];
-    int population2;
-    float area2;
-    float gdp2;
-    int touristSpots2;
+    char state1, state2;
+    char code1[4], code2[4];
+    char city1[100], city2[100];
+    int population1, population2;
+    float area1, area2;
+    float gdp1, gdp2;
+    int touristSpots1, touristSpots2;
+    
+    float density1, density2;
+    float gdpPerCapita1, gdpPerCapita2;
 
     // Input for card 1
     printf("Enter the details for Card 1:\n");
@@ -24,7 +19,7 @@ int main() {
     printf("Card Code: ");
     scanf("%s", code1);
     printf("City Name: ");
-    scanf(" %[^\n]", city1);
+    scanf(" %[^"]", city1);
     printf("Population: ");
     scanf("%d", &population1);
     printf("Area (in km²): ");
@@ -41,7 +36,7 @@ int main() {
     printf("Card Code: ");
     scanf("%s", code2);
     printf("City Name: ");
-    scanf(" %[^\n]", city2);
+    scanf(" %[^"]", city2);
     printf("Population: ");
     scanf("%d", &population2);
     printf("Area (in km²): ");
@@ -50,6 +45,12 @@ int main() {
     scanf("%f", &gdp2);
     printf("Number of Tourist Spots: ");
     scanf("%d", &touristSpots2);
+    
+    // Calculating density and GDP per capita
+    density1 = population1 / area1;
+    density2 = population2 / area2;
+    gdpPerCapita1 = gdp1 * 1000000000 / population1; // Convert billions to reais
+    gdpPerCapita2 = gdp2 * 1000000000 / population2;
 
     // Displaying the data
     printf("\nCard 1:\n");
@@ -60,6 +61,8 @@ int main() {
     printf("Area: %.2f km²\n", area1);
     printf("GDP: %.2f billion reais\n", gdp1);
     printf("Number of Tourist Spots: %d\n", touristSpots1);
+    printf("Density: %.2f hab/km²\n", density1);
+    printf("GDP per Capita: %.2f reais\n", gdpPerCapita1);
 
     printf("\nCard 2:\n");
     printf("State: %c\n", state2);
@@ -69,6 +72,8 @@ int main() {
     printf("Area: %.2f km²\n", area2);
     printf("GDP: %.2f billion reais\n", gdp2);
     printf("Number of Tourist Spots: %d\n", touristSpots2);
+    printf("Density: %.2f hab/km²\n", density2);
+    printf("GDP per Capita: %.2f reais\n", gdpPerCapita2);
 
     return 0;
 }
